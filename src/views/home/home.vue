5cebf86562c9b666ea203befcb8996c137e7bd01
Sixpence <1239170998@qq.com>
2021-08-20 22:42:05 +0800

    tab-control

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -4,14 +4,21 @@
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends" />
     <feature-view></feature-view>
+    <tab-control
+      :titles="['流行', '新款', '精选']"
+      class="tabcontrol"
+    ></tab-control>
   </div>
 </template>
 
 <script>
-import NavBar from "../../components/common/navbar/NavBar.vue";
 import HomeSwiper from "./childComps/HomeSwiper.vue";
 import RecommendView from "./childComps/RecommendView.vue";
 import featureView from "./childComps/featureView.vue";
+
+import NavBar from "../../components/common/navbar/NavBar.vue";
+import TabControl from "../../components/content/tabControl/TabControl.vue";
+
 import { getHomeMultidata } from "../../network/home";
 
 export default {
@@ -20,6 +27,7 @@ export default {
     HomeSwiper,
     RecommendView,
     featureView,
+    TabControl,
   },
   data() {
     return {
@@ -37,8 +45,21 @@ export default {
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+  height: 3000px;
+}
 .home-nav {
   background-color: var(--color-tint);
   color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+}
+.tabcontrol {
+  position: sticky;
+  top: 44px;
 }
 </style>
